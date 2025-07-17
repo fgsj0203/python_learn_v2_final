22d970ceec690f8e1f53995fbed5ece9a5943ca1
Francisco Junior <fgsj.dev@outlook.com>
2025-07-17 10:55:09 -0300

    feat: adding validation data user input for only numbers positive

--- sequential_structure/exercise08.py
+++ sequential_structure/exercise08.py
@@ -1,3 +1,4 @@
+# --------------------------------------------------------------------------------------------------------
 number_one = int(input("one: "))
 number_two = int(input("two: "))
 number_three = float(input("three: "))
@@ -10,6 +11,7 @@ operation_three = number_three**3
 print("operation one: %.2f" % operation_one)
 print("\noperation two: %.2f" % operation_two)
 print("\noperation three: %.2f" % operation_three)
+# --------------------------------------------------------------------------------------------------------
 
 
 # Solution with function
@@ -26,3 +28,28 @@ def operations_numbers(number_one_function, number_two_function, number_three_fu
 
 
 operations_numbers(40, 41, 3.14)
+# --------------------------------------------------------------------------------------------------------
+
+
+# Solution with validation of data input user
+def operations_numbers_valid(number_one_valid, number_two_valid, number_three_valid):
+    print("Solution with validation data user input")
+    while number_one_valid < 0:
+        print("number invalid, try again!")
+        number_one_valid = int(input("number one: "))
+    while number_two_valid < 0:
+        print("number invalid, try again!")
+        number_two_valid = int(input("number two: "))
+    while number_three_valid < 0:
+        print("number invalid, try again!")
+        number_three_valid = float(input("number three: "))
+    operation_one = (number_one_valid * 2) * (number_two_valid / 2)
+    operation_two = (number_one_valid * 3) + number_three_valid
+    operation_three = number_three_valid**3
+
+    # Printing values final of operations
+    print("operation one: %.2f" % operation_one)
+    print("\noperation two: %.2f" % operation_two)
+    print("\noperation three: %.2f" % operation_three)
+
+operations_numbers_valid(-1,3,-2)
