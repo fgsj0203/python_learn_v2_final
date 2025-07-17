bbd41c7a486669d41dec6656dc1642e7a9d6e513
Francisco Junior <fgsj.dev@outlook.com>
2025-07-17 10:15:11 -0300

    exercise area of circle with validation data for number negative

--- sequential_structure/exercise06.py
+++ sequential_structure/exercise06.py
@@ -4,6 +4,7 @@ formula_area_circle = PI * (raid_circle**2)
 print("The area of circle is: %.3f" % formula_area_circle)
 
 
+# -------------------------------------------------------------------------------------
 # Solution with function
 def area_circle(raid_circle):
     print("Solution with function")
@@ -12,3 +13,20 @@ def area_circle(raid_circle):
 
 
 area_circle(1)
+# -------------------------------------------------------------------------------------
+
+
+# -------------------------------------------------------------------------------------
+# Solution with function and validation data
+def area_circle_validation(raid_circle):
+    print("Solution with validation")
+    while raid_circle < 0:
+        print("Value of raid invalid, try again!")
+        raid_circle = float(input("new value of raid circle: "))
+
+    formula_area_circle = PI * (raid_circle**2)
+    print("The area of circle is: %.3f" % formula_area_circle)
+
+
+area_circle_validation(-1)
+# -------------------------------------------------------------------------------------
