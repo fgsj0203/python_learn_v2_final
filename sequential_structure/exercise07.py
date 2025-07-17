68a16a311790e7968cb27f75c3825888a187e473
Francisco Junior <fgsj.dev@outlook.com>
2025-07-17 10:36:11 -0300

    fix: adding function with validation of value data input of user

--- sequential_structure/exercise07.py
+++ sequential_structure/exercise07.py
@@ -2,3 +2,35 @@ side_square = float(input("Size of side square: "))
 area_square = side_square * side_square
 print("The value of side square with %.1f is area %.2f" % (side_square, area_square))
 print("\nThe value double of result area square is: %.2f " % (area_square * 2))
+
+
+# Solution with function
+def area_square_function(side_square_function):
+    print("\nSolution with function")
+    area_square_function = side_square_function * side_square_function
+    print("\nThe value of area square is %.2f " % area_square_function)
+    print(
+        "\nThe value double of result previous of square is: %.2f "
+        % (area_square_function * 2)
+    )
+
+
+# Calling function
+area_square_function(3)
+
+
+# Solution with function and validation data
+def area_square_validation(side_square_validation):
+    print("\nSolution with validation data")
+    while side_square_validation < 0:
+        print("\nvalue of side is invalid, try again!")
+        side_square_validation = float(input("New value and valid of side square: "))
+    area_square_valid = side_square_validation * side_square_validation
+    print("\nThe value of area square is: %.2f " % area_square_valid)
+    print(
+        "\nThe value double of original result square is: %.2f "
+        % (area_square_valid * 2)
+    )
+
+
+area_square_validation(-1)
