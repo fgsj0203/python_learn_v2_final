37469a7c23540b0555392b17c09037b07f1ef51b
Francisco Junior <fgsj.dev@outlook.com>
2025-07-17 15:51:05 -0300

    feat: bigger of two number

---
+++ structure_decision/exercise01.py
@@ -0,0 +1,6 @@
+number_one = int(input("number one: "))
+number_two = int(input("number two: "))
+if number_one > number_two:
+    print("The number one is %d is bigger" % number_one)
+else:
+    print("The number two is %d is bigger" % number_two)
