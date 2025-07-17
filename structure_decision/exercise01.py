767ef30b4d864357634e0d6181c5fe75e4b891a3
Francisco Junior <fgsj.dev@outlook.com>
2025-07-17 16:23:44 -0300

    feat:  adding a solution using function

--- structure_decision/exercise01.py
+++ structure_decision/exercise01.py
@@ -4,3 +4,16 @@ if number_one > number_two:
     print("The number one is %d is bigger" % number_one)
 else:
     print("The number two is %d is bigger" % number_two)
+
+
+# Solution with function
+
+
+def bigger_number(number_one, number_two):
+    if number_one > number_two:
+        print("The number one is %d is bigger" % number_one)
+    else:
+        print("The number two is %d is bigger" % number_two)
+
+
+bigger_number(-10, 5)
