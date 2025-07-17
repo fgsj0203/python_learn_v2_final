f329cd44daa9df41dcf08ba28be6e8898c9d49bc
Francisco Junior <fgsj.dev@outlook.com>
2025-07-17 10:21:27 -0300

    calculate area of square and printing double of result original

---
+++ sequential_structure/exercise07.py
@@ -0,0 +1,4 @@
+side_square = float(input("Size of side square: "))
+area_square = side_square * side_square
+print("The value of side square with %.1f is area %.2f" % (side_square, area_square))
+print("\nThe value double of result area square is: %.2f " % (area_square * 2))
