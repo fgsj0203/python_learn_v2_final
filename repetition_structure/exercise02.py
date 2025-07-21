8d62f905e2160a43c6dcee05bbeedd27fd5c2749
Francisco Junior <fgsj.dev@outlook.com>
2025-07-21 12:01:22 -0300

    update: creating function for solution with parameters

--- repetition_structure/exercise02.py
+++ repetition_structure/exercise02.py
@@ -8,6 +8,24 @@ while name_user == password_user:
     password_user = str(input("Password: "))
 
 print("------------------ Print final -------------------")
-print("\nResult public final\n")
 print("Your name user is: %s " % name_user)
 print("Your password is: %s " % password_user)
+print("------------------ Print final -------------------")
+
+# ---------------------------------------------------------------------------------------------
+
+
+# Creating function with parameter
+def validation_password_user(name_user, password):
+    while name_user == password:
+        print("Solution with parameters \n")
+        print("name and password is equal, change values")
+        name_user = str(input("Name user: "))
+        password = str(input("Password: "))
+    print("\nResult final \n")
+    print(name_user)
+    print(password)
+
+
+# Calling function with paramter
+validation_password_user("francisco", "francisco")
