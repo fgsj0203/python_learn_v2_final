587ef9e959d4427abe420e683dac6036d17cf066
Francisco Junior <fgsj.dev@outlook.com>
2025-07-22 09:54:11 -0300

    update: adding function for equal solution growthing population A and B

--- repetition_structure/exercise04.py
+++ repetition_structure/exercise04.py
@@ -12,3 +12,24 @@ while population_a <= population_b:
 print("The final growthing population A is %.2f" % population_a)
 print("The final growthing population B is %.2f" % population_b)
 print("After %d years the population A is bigger population B" % counter_year)
+
+# Using function for equal solution
+
+
+def growthing_population():
+    first_population = 80000
+    second_population = 200000
+    first_growthing = 0.03
+    second_growthing = 0.015
+    counter_year = 0
+    while first_population <= second_population:
+        first_population = first_population + (first_population * first_growthing)
+        second_population = second_population + (second_population * second_growthing)
+        counter_year += 1
+
+    print(
+        "After %d years the population A with %.2f is bigger population B with %.2f "
+        % counter_year,
+        population_a,
+        population_b,
+    )
