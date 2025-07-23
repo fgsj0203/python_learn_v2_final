442f05a1187287f96b8a580b0150c71673eb15c3
Francisco Junior <fgsj.dev@outlook.com>
2025-07-23 10:04:04 -0300

    feat: creating second class with most attributes

--- OOP/exercise01.py
+++ OOP/exercise01.py
@@ -8,6 +8,15 @@ class People:
         self.name = name
         self.age = age
 
+class PeopleTwo:
+    def __init__(self, firstName, lastName, age, address):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+        self.address = address
+
+other_people = PeopleTwo("Francisco", "junior", 30, "Rua Onze de Fevereiro")
+print(f"I {other_people.firstName} with {other_people.lastName} have {other_people.age} and living {other_people.address}")
 
 people_one = People("Francisco", 30)
 print(f"{people_one.name} and {people_one.age} age")
