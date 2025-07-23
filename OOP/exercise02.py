0a0b8982f8fc7026588814b0b256ea7e2265d0eb
Francisco Junior <fgsj.dev@outlook.com>
2025-07-23 10:23:50 -0300

    update: insert lines comments in code

--- OOP/exercise02.py
+++ OOP/exercise02.py
@@ -3,6 +3,7 @@ Creating class with method (leve beginner)
 """
 
 
+# Creating class with attributes
 class People:
     def __init__(self, firstName, lastName, age, address):
         self.firstName = firstName
@@ -10,10 +11,13 @@ class People:
         self.age = age
         self.address = address
 
+    # Creating first method for print values
     def printDataPeople(self):
         print(
             f"Hello, I'm {self.firstName} and {self.lastName} with {self.age} and finally living in {self.address}"
         )
+
+    # Creating second method for print data people
     def printAge(self):
         print(f"Hello, i'm have {self.age}")
 
