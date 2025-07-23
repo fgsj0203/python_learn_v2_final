d3ed0a8a2e1dc25f75a9f3de9f875a100af30f9c
Francisco Junior <fgsj.dev@outlook.com>
2025-07-23 10:18:27 -0300

    feat: creating class with method for printing values

---
+++ OOP/exercise02.py
@@ -0,0 +1,23 @@
+"""
+Creating class with method (leve beginner)
+"""
+
+
+class People:
+    def __init__(self, firstName, lastName, age, address):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+        self.address = address
+
+    def printDataPeople(self):
+        print(
+            f"Hello, I'm {self.firstName} and {self.lastName} with {self.age} and finally living in {self.address}"
+        )
+    def printAge(self):
+        print(f"Hello, i'm have {self.age}")
+
+
+p1 = People("Francisco", "Junior", 30, "Rua Onze de fevereiro")
+p1.printDataPeople()
+p1.printAge()
