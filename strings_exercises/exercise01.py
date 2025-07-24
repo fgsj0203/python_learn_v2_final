71b5e07df34d25b516f42af6a1f3c3b2ec60c425
Francisco Junior <fgsj.dev@outlook.com>
2025-07-24 10:37:26 -0300

    update: create function for calculate size string without builtin function len()

--- strings_exercises/exercise01.py
+++ strings_exercises/exercise01.py
@@ -5,3 +5,22 @@ Calculate string lenght
 string = str(input("string: "))
 size_string = len(string)
 print("The size of string is: %d" % size_string)
+
+
+# -----------------------------------------------------------
+"""
+Creating solution without function builtin len()
+"""
+
+
+def sizeString(str1):
+    count_string = 0
+
+    for x in str1:
+        count_string += 1
+
+    return count_string
+
+
+print(sizeString("francisco"))
+# -----------------------------------------------------------
